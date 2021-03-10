39036836764707ffb3d060704b827df39b717c5a
hawkishnes <61586825+hawkishnes@users.noreply.github.com>
2021-03-10 11:10:34 -0500

    Almost finished script

--- Scripts/Searchersricpt.ps1
+++ Scripts/Searchersricpt.ps1
@@ -2,3 +2,7 @@ Clear-Content "..\Inputsearchquery.txt"
 Clear-Content "../searchquerywithoutspaces.txt"
 
 $search = read-host "Enter Search Query"
+
+$SavePath = "Inputsearchquery.txt"
+
+Set-Content -Path (SavePath) -Value (search) -Force 
