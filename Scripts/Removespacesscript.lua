2b8b043c92a3187346bc6d62c17dc9c6347e4bbf
hawkishnes <61586825+hawkishnes@users.noreply.github.com>
2021-03-10 10:37:16 -0500

    Now checks input and fixed a bug

--- Scripts/Removespacesscript.lua
+++ Scripts/Removespacesscript.lua
@@ -1,8 +1,7 @@
-InputQuery =  
+InputQuery = io.open("Inputsearchquery", "r")
 02
 03	-- Replace spaces with empty text
 04	FunalSearchQuery = InputQuery:gsub('%s+', '+')
 05	 
-06	print(io.open("searchquerywithoutspaces", "w"))
-07    --> nil     No such file or directory       2
-08  f:write(FunalSearchQuery)
+06  io.open("searchquerywithoutspaces", "w")
+07  f:write(FunalSearchQuery)
