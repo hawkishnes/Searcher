4c2caadfde58faaaa4b4dce86977a35dc48e58ac
hawkishnes <61586825+hawkishnes@users.noreply.github.com>
2021-03-10 11:33:27 -0500

    Fixed Spelling Error

--- Scripts/Removespacesscript.lua
+++ Scripts/Removespacesscript.lua
@@ -1,7 +1,7 @@
 InputQuery = io.open("Inputsearchquery", "r")
 02
 03	-- Replace spaces with empty text
-04	FunalSearchQuery = InputQuery:gsub('%s+', '+')
+04	FinalSearchQuery = InputQuery:gsub('%s+', '+')
 05	 
 06  io.open("searchquerywithoutspaces", "w")
-07  f:write(FunalSearchQuery)
+07  f:write(FinalSearchQuery)
