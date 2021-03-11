d13137efc8b59190f8902d523f129f0c8b3e559d
Hawk Nest <61586825+hawkishnes@users.noreply.github.com>
2021-03-11 18:44:13 -0500

    Hopefully, fixed issue ( #3 )

--- Scripts/Removespacesscript.lua
+++ Scripts/Removespacesscript.lua
@@ -1,9 +1,8 @@
-InputQuery = io.open("Inputsearchquery", "r")
+InputQuery = io.open("../Inputsearchquery", "r")
 02
-03	-- Replace spaces with empty text
-04	FinalSearchQuery = InputQuery:gsub('%s+', '+')
-05	 
-06  io.open("searchquerywithoutspaces", "w")
-07  f:write("Https://google.com/search=?"FinalSearchQuery)
-08 
-09  & "../Openurlscript.lua"
+03	FinalSearchQuery = InputQuery:gsub('%s+', '+')
+04	 
+05  io.open("../searchquerywithoutspaces", "w")
+06  f:write("Https://google.com/search=?"FinalSearchQuery)
+07 
+08  & "../Openurlscript.lua"
