44834f4d4f2a29df44881f61a995719873b5c2d1
Hawk Nest <61586825+hawkishnes@users.noreply.github.com>
2021-03-14 07:07:37 -0400

    Hopefully fixed confusion between languages

--- Scripts/Openurlscript.lua
+++ Scripts/Openurlscript.lua
@@ -5,7 +5,7 @@ function open_url(url)
     if not open_cmd then
         if package.config:sub(1,1) == '\\' then 
             open_cmd = function(url)
-                os.execute(string.format('start "%s"',(io.open("../Resources/searchquerywithoutspaces", "r")))
+                os.execute(string.format('start "%s"',(io.open("Resources/searchquerywithoutspaces", "r")))
             end
         end
     end
