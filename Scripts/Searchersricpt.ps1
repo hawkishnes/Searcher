2d4b48414df4a4a50325fffb6cfa8323521e0118
Hawk Nest <61586825+hawkishnes@users.noreply.github.com>
2021-04-05 16:44:44 -0400

    Added Savable Safe Search option

--- Scripts/Searchersricpt.ps1
+++ Scripts/Searchersricpt.ps1
@@ -1,5 +1,6 @@
 Clear-Content "../Resources/Inputsearchquery.txt"
 Clear-Content "../Resources/searchquerywithoutspaces.txt"
+Clear-Content "../Resources/ssa.txt"
 
 $search = read-host "Enter Search Query"
 $safesearch = read-host "Safe Search Ative"
@@ -8,7 +9,10 @@ $safesearch = read-host "Safe Search Ative"
 # Set the second as "image" to have off
 
 $SavePath = "../Resources/Inputsearchquery.txt"
+$SavePath2 = "../Resources/ssa.txt"
 
 Set-Content -Path (SavePath) -Value (search) -Force 
 
 & '../Removespacesscript.lua'
+
+Set-Content -Path (SavePath2) -Value (safesearch) -Force
