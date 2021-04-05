c04969b32a5fc2191f5734a72693841d33b5cf1e
Hawk Nest <61586825+hawkishnes@users.noreply.github.com>
2021-04-05 16:25:26 -0400

    Added “Safe Search Activate” Option (#9)

--- Scripts/Searchersricpt.ps1
+++ Scripts/Searchersricpt.ps1
@@ -2,6 +2,10 @@ Clear-Content "../Resources/Inputsearchquery.txt"
 Clear-Content "../Resources/searchquerywithoutspaces.txt"
 
 $search = read-host "Enter Search Query"
+$safesearch = read-host "Safe Search Ative"
+
+# Set the second as "active" to have on
+# Set the second as "image" to have off
 
 $SavePath = "../Resources/Inputsearchquery.txt"
 
