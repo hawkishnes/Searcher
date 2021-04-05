31374b2e01cf2cb51bbebf33adba7bbe3adbd1a0
Hawk Nest <61586825+hawkishnes@users.noreply.github.com>
2021-04-05 16:48:19 -0400

    Added usable safe search, hopefully

--- Scripts/Removespacesscript.lua
+++ Scripts/Removespacesscript.lua
@@ -1,8 +1,10 @@
-InputQuery = io.open("Resources/Inputsearchquery", "r")
+local savesearch = io.open("Resources/ssa.txt"
+
+InputQuery = io.open("Resources/Inputsearchquery.txt", "r")
 02
 03	FinalSearchQuery = InputQuery:gsub('%s+', '+')
 04	 
-05  io.open("Resources/searchquerywithoutspaces", "w")
-06  f:write("Https://google.com/search=?"FinalSearchQuery)
+05  io.open("Resources/searchquerywithoutspaces.txt", "w")
+06  f:write("Https://google.com/search=?"FinalSearchQuery"&save="safesearch)
 07 
 08  & "Openurlscript.lua"
